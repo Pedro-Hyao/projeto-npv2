cee1886e3b2c0549c80ba0d3b17cefeb2d58db33
Pedro-Hyao <pamonha0222@gmail.com>
2024-11-21 16:38:13 -0300

    Update Bandass.jsx

--- src/Bandass.jsx
+++ src/Bandass.jsx
@@ -9,7 +9,7 @@ function Bandass() {
     
   <Box bgcolor="" width="87%" height="100%"
   display="flex" justifyContent="center" alignItems="start"
-  flexDirection="column" fontSize="32px">
+  flexDirection="column" fontSize="32px" bgcolor="white">
   <h2>Bandas Importantes na Música paulista.</h2>
   <p>Racionais MC's</p>
 
