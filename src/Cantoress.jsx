18bf54056e7ff1ff180f7758980f1eee59438ce9
Pedro-Hyao <pamonha0222@gmail.com>
2024-11-21 16:37:56 -0300

    Update Cantoress.jsx

--- src/Cantoress.jsx
+++ src/Cantoress.jsx
@@ -11,7 +11,7 @@ function Cantoress() {
     bgcolor="" width="87%" height="100%"
       display="flex" justifyContent="space-around" alignItems="start"
       flexDirection="column" fontSize="12px"
-      color="black">
+      color="black" bgcolor="white">
 
 <Pagina />
       
